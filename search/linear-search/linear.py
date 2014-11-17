18f6300f5a98892d5cd060c78c55997ae9a6e17b
Ritwik Dutta <ritzymail@gmail.com>
2014-11-16 21:51:38 -0800

    Updating example.
    I ran the example to confirm it worked. I also implemented a linear search wrapper function.

--- search/linear-search/linear.py
+++ search/linear-search/linear.py
@@ -3,8 +3,6 @@ Function: find_item_in_list
 Input: item_to_find (variable), item_list (list)
 Output: Item index if found, or -1 if the item is not in the list
 '''
-
-#Find item in a list
 def find_item_in_list(item_to_find, item_list):
   #Iterate through list
   for i in range(len(item_list)):
@@ -14,4 +12,37 @@ def find_item_in_list(item_to_find, item_list):
       return i
   #Since item was never found, return -1
   return -1
-    
+
+'''
+Function: linear_search
+Input: item_to_find, variable, item_list (list)
+Output: Sentence stating result of search.
+'''
+def linear_search(item_to_find, item_list):
+    item_index = find_item_in_list(item_to_find, item_list)
+    if item_index == -1:
+        print "Item not found in list."
+    else:
+        print "Item was found in list at position " + str(item_index) + "."
+
+
+#Linear search case 1
+item_list = [4, 7, 12, 44, 2]
+item_to_find = 12
+linear_search(item_to_find, item_list)
+'''
+Function output: 
+
+Item was found in list at position 2.
+'''
+
+#Linear search case 1
+item_list = [12,42,8,95]
+item_to_find = 120
+linear_search(item_to_find, item_list)
+'''
+Function output: 
+
+Item was not found in list.
+'''
+
