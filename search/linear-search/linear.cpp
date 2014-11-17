816ae7bf84970e1ba369f0c6cf30f27cc57e28dd
Ritwik Dutta <ritzymail@gmail.com>
2014-11-16 22:16:06 -0800

    Updating example.
    I updated  the example with a linear search wrapper and added in some more examples.

--- search/linear-search/linear.cpp
+++ search/linear-search/linear.cpp
@@ -24,17 +24,31 @@ int findItemInList(float itemToFind, float itemList[], int arrayLength) {
 void linearSearch(float itemToFind, float itemList[], int arrayLength) {
 	int itemIndex = findItemInList(itemToFind, itemList, arrayLength);
 	if (itemIndex == -1) {
-		cout << "Item was not found in list.";
+		cout << "Item was not found in list." << endl;
 	} else {
-		cout << "Item was found in list at position " << itemIndex << ".";
+		cout << "Item was found in list at position " << itemIndex << "." << endl;
 	}
 }
 
 int main()
 {
+   //Linear search example 1
    float itemList[5] = {1.0,76.0,93.0,51.0,42.0};
    float itemToFind = 93.0;
    int arrayLength = 5;
    linearSearch(itemToFind, itemList, arrayLength);
+   //Function output:
+   //
+   //Item was found in list at position 2.
+
+   //Linear search example 2
+   float itemList2[5] = {16.0,7.0,239.0,52.0,2.0};
+   float itemToFind2 = 64.0;
+   int arrayLength2 = 5;
+   linearSearch(itemToFind2, itemList2, arrayLength2);
+   //Function output:
+   //
+   //Item was not found in list..
+   
    return 0;
 }
