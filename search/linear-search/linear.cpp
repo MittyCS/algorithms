b272d77157a8f3e6371c9682acfb593e9f0cfff2
Ritwik Dutta <ritzymail@gmail.com>
2014-11-16 22:13:35 -0800

    Update linear.cpp

--- search/linear-search/linear.cpp
+++ search/linear-search/linear.cpp
@@ -6,13 +6,28 @@ using namespace std;
 //Input: itemToFind (float), itemList (float[]), arrayLength[int]
 //Output: Index of item in list, or -1 if the item is not in the list
 int findItemInList(float itemToFind, float itemList[], int arrayLength) {
-	//Iterate through
+	//Iterate through array
     for (int i = 0; i < arrayLength; i++) {
+    	//Check if current item is the item we are looking for
         if (itemList[i] == itemToFind) {
+        	//Return index of item and end function
             return i;
         }
     }
+    //If item is not found, return -1
     return -1;
+}
+
+//Function: linearSearch
+//Input: itemToFind (float), itemList (float[]), arrayLength (int)
+//Output: Sentence stating result of search.
+void linearSearch(float itemToFind, float itemList[], int arrayLength) {
+	int itemIndex = findItemInList(itemToFind, itemList, arrayLength);
+	if (itemIndex == -1) {
+		cout << "Item was not found in list.";
+	} else {
+		cout << "Item was found in list at position " << itemIndex << ".";
+	}
 }
 
 int main()
@@ -20,10 +35,6 @@ int main()
    float itemList[5] = {1.0,76.0,93.0,51.0,42.0};
    float itemToFind = 93.0;
    int arrayLength = 5;
-   
-   int itemIndex = findItemInList(itemToFind, itemList, arrayLength);
-   
-    cout << itemIndex;
-      
+   linearSearch(itemToFind, itemList, arrayLength);
    return 0;
 }
