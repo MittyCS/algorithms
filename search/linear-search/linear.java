93bb74c7bca3bf84cac73264012410cf6f9f4102
Ritwik Dutta <ritzymail@gmail.com>
2014-11-16 22:02:09 -0800

    Updating example.
    I added a linear search wrapper function and confirmed that it compiles and works.

--- search/linear-search/linear.java
+++ search/linear-search/linear.java
@@ -1,20 +1,51 @@
 public class linear {
-  public static void main(String args[]) {
-  }
-  
-  //Function: findItemInList
-  //Input: itemToFind (Object), itemList (ArrayList<Object>)
-  //Output: Index of item in list, or -1 if the item is not in the list
-  public static int findItemInList(Object itemToFind, ArrayList<Object> itemList) {
-    //Iterate through list
-    for (int i = 0; i < itemList.size(); i++) {
-      //Check if current item is item we are looking for
-      if (itemList.get(i).equals(itemToFind)) {
-        //Return index of item and finish function
-        return i;
-      }
+    public static void main(String args[]) {
+        
+      //Linear search example 1    
+      double itemToFind = 5;
+      double[] itemList = {6, 8, 12, 5, 12};
+      linearSearch(itemToFind, itemList);
+      //Function output:
+      //
+      //Item was found in list at position 3.
+      
+      //Linear search example 2
+      double itemToFind2 = 85;
+      double[] itemList2 = {7,91,65,23,76};
+      linearSearch(itemToFind2, itemList2);
+      //Function output:
+      //
+      //Item was not found in list.
+      
     }
-    //If no item is found, return -1
-    return -1;
-  }
+    
+    //Function: findItemInList
+    //Input: itemToFind (double), itemList (double[])
+    //Output: Index of item in list, or -1 if the item is not in the list
+    public static int findItemInList(double itemToFind, double[] itemList) {
+        //Iterate through list
+        for (int i = 0; i < itemList.length; i++) {
+          //Check if current item is item we are looking for
+          if (itemList[i] == itemToFind) {
+            //Return index of item and finish function
+            return i;
+          }
+        }
+        //If no item is found, return -1
+        return -1;
+    }
+  
+    //Function: linearSearch
+    //Input: itemToFind (double), itemList (double[])
+    //Output: Sentence stating result of search.
+    public static void linearSearch(double itemToFind, double[] itemList) {
+        int itemIndex = findItemInList(itemToFind, itemList);
+        if (itemIndex == -1) {
+            System.out.println("Item was not found in list.");
+        } else {
+            System.out.println("Item was found in list at position " + Integer.toString(itemIndex) + ".");
+        }
+    }
+
+  
 }
